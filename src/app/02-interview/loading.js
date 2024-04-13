666882a8a59e31d5347a55ac63ce08ae0e7aa85c
Adjel <adjel.touaoula@gmail.com>
2024-04-13 13:48:29 +0200

    first commit

--- src/app/02-interview/loading.js
+++ src/app/02-interview/loading.js
@@ -1,13 +1,7 @@
 import React from "react";
-
-import { getComments } from "@/helpers/interview-helpers";
-import Comment from "@/components/Comment";
-
-export const dynamic = "force-dynamic";
+import Spinner from "@/components/Spinner";
 
 async function InterviewExercise() {
-  const comments = await getComments();
-
   return (
     <>
       <article className="main-article">
@@ -100,9 +94,8 @@ async function InterviewExercise() {
 
       <section className="comments-section">
         <h2>Discussion</h2>
-        {comments.map((comment) => (
-          <Comment key={comment.id} comment={comment} />
-        ))}
+
+        <Spinner />
       </section>
     </>
   );
