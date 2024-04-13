059961697c5c72d3fcd8a4e90eef627612295e2f
Adjel <adjel.touaoula@gmail.com>
2024-04-13 14:50:51 +0200

    correction

--- src/app/02-interview/page.js
+++ src/app/02-interview/page.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import { getComments } from "@/helpers/interview-helpers";
 import Comment from "@/components/Comment";
+import Spinner from "@/components/Spinner";
 
 export const dynamic = "force-dynamic";
 
 async function InterviewExercise() {
-  const comments = await getComments();
-
   return (
     <>
       <article className="main-article">
@@ -100,12 +99,20 @@ async function InterviewExercise() {
 
       <section className="comments-section">
         <h2>Discussion</h2>
-        {comments.map((comment) => (
-          <Comment key={comment.id} comment={comment} />
-        ))}
+        <React.Suspense fallback={<Spinner />}>
+          <Comments />
+        </React.Suspense>
       </section>
     </>
   );
+}
+
+async function Comments() {
+  const comments = await getComments();
+
+  return comments.map((comment) => (
+    <Comment key={comment.id} comment={comment} />
+  ));
 }
 
 export default InterviewExercise;
