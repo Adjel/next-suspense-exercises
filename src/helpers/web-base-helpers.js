e4d9223e2ad34bc4182167745d820980e51ac4b4
Adjel <adjel.touaoula@gmail.com>
2024-04-13 15:57:31 +0200

    correction

--- src/helpers/web-base-helpers.js
+++ src/helpers/web-base-helpers.js
@@ -1,44 +1,39 @@
-import React from 'react';
+import React from "react";
 
-export async function getNavLinks() {
-  console.info(
-    'Requesting navigation links from CMS'
-  );
+export const getNavLinks = React.cache(async function getNavLinks() {
+  console.info("Requesting navigation links from CMS");
   await delay(1800);
 
   return LINKS;
-}
+});
 
-const delay = (ms) =>
-  new Promise((resolve) =>
-    setTimeout(resolve, ms)
-  );
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const LINKS = [
   {
-    slug: 'platform',
-    label: 'Platform',
-    href: '/platform',
-    type: 'primary',
+    slug: "platform",
+    label: "Platform",
+    href: "/platform",
+    type: "primary",
   },
   {
-    slug: 'solutions',
-    label: 'Solutions',
-    href: '/solutions',
+    slug: "solutions",
+    label: "Solutions",
+    href: "/solutions",
   },
   {
-    slug: 'integrations',
-    label: 'Integrations',
-    href: '/integrations',
+    slug: "integrations",
+    label: "Integrations",
+    href: "/integrations",
   },
   {
-    slug: 'docs',
-    label: 'Docs',
-    href: '/documentation/start-here',
+    slug: "docs",
+    label: "Docs",
+    href: "/documentation/start-here",
   },
   {
-    slug: 'pricing',
-    label: 'Pricing',
-    href: '/pricing',
+    slug: "pricing",
+    label: "Pricing",
+    href: "/pricing",
   },
 ];
